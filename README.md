2b2c1fb17928f524560e00ab10e0bbdb9bc4844d
Ahmed Alaa <engahmedalaamo@gmail.com>
2023-11-24 15:06:25 +0200

    New updates

--- README.md
+++ README.md
@@ -2,15 +2,39 @@
 
 Single Page Website With Multi Sections Responsive Across All Devices With Some Dynamic Added To It.
 
+Check the website **[Live](https://engahmedalaa.github.io/template-design-two/)**.
+
+## Technologies used
+
+- HTML
+
+- CSS
+
+- JavaScript
+
 ## Main Sections:
 
--   Header Section
--   Home Section
--   Services Section
--   Porfolio Section
--   About Section
--   Pricing Section
--   Contact Section
--   Footer Section
+- Home section
+- Services section
+- Design section
+- Portfolio section
+- Video section
+- About us section
+- Stats section
+- Us section
+- Quote section
+- Pricing section
+- Subscribe section
+- Contact section
+
+### Packages used
+
+- Swiper
+
+- Mixitup
+
+- Countup
+
+- wow
 
 The Design is By Kasper at [GRAPHBERRY](https://www.graphberry.com/item/kasper-one-page-psd-template).
--- js/app.js
+++ js/app.js
@@ -1,32 +1,216 @@
 // Global Variables
-// Burger Toggle Menu
-const toggleMeue = document.querySelector('header .toggle-menu');
-// Link list
-const linkList = document.getElementById('menu-navbar');
-// Scroll To Top Button
-const toTopButton = document.querySelector('.up');
+const mainHeader = document.getElementById('main-header');
 
-// Main Function
-// Showing And Hidding The Scroll To Top Button
-function scrollToTopControl() {
-    if (window.scrollY >= 1000) {
-        toTopButton.classList.add('show');
-    } else {
-        toTopButton.classList.remove('show');
-    }
+// NavBar
+const navBar = document.getElementById('nav-bar');
+
+// NavBar Trigger
+const navBarTrigger = navBar.querySelector('.nav-bar-trigger');
+
+// NavBar Menu
+const navBarMenu = navBar.querySelector('.nav-bar-menu');
+
+// NavBar Links
+const navBarLinks = [...navBarMenu.querySelectorAll('.nav-bar-link')];
+
+// sections
+const sections = [...document.querySelectorAll('section[data-link]')];
+
+// Scroll To Top Button
+const scrollToTopButton = document.getElementById('scroll-to-top-button');
+
+// Main Functions
+// Function Control Main Header Showing Hiding
+function controlMainHeader() {
+  if (window.scrollY >= 40) {
+    mainHeader.classList.add('scrolled');
+  } else {
+    mainHeader.classList.remove('scrolled');
+  }
+}
+controlMainHeader();
+
+// Function Add Close Class NavBar Menu
+function addClosedClass() {
+  navBarMenu.classList.add('closed');
 }
 
+// Function Open NavBar Menu
+function openNavBarMenu() {
+  navBarMenu.removeEventListener('animationend', addClosedClass);
+  navBarTrigger.setAttribute('data-state', 'open');
+  navBarTrigger.setAttribute('aria-expanded', 'true');
+  navBarMenu.setAttribute('data-state', 'open');
+  navBarMenu.classList.remove('closed');
+}
+
+// Function Close NavBar Menu
+function closeNavBarMenu() {
+  navBarTrigger.setAttribute('data-state', 'closed');
+  navBarTrigger.setAttribute('aria-expanded', 'false');
+  navBarMenu.setAttribute('data-state', 'closed');
+  navBarMenu.addEventListener('animationend', addClosedClass, {
+    once: true,
+  });
+}
+
+// Function Add Active State To Nav Links On Section Appearing
+function activateNavLinks() {
+  sections.forEach((section) => {
+    const halfScreen = window.innerHeight * 0.5;
+    const isVisible =
+      section.getBoundingClientRect().top < halfScreen &&
+      section.getBoundingClientRect().bottom > halfScreen;
+    const navLink = navBarLinks.find(
+      (navLink) => navLink.getAttribute('data-section') === section.id
+    );
+    if (isVisible) navLink.setAttribute('data-state', 'active');
+    else navLink.removeAttribute('data-state', 'active');
+  });
+}
+activateNavLinks();
+
+// Function Active Lazy Video On Appearing
+function activateLazyVideo() {
+  const lazyVideoElement = document.querySelector('.lazy-video');
+  if (!lazyVideoElement)
+    return window.removeEventListener('scroll', activateLazyVideo);
+  const rect = lazyVideoElement.getBoundingClientRect();
+  const isVisible = window.innerHeight > rect.top && 0 < rect.bottom;
+  if (!isVisible) return;
+  const videoSource = lazyVideoElement.children[0];
+  videoSource.src = videoSource.dataset.src;
+  lazyVideoElement.load();
+  lazyVideoElement.classList.remove('lazy-video');
+}
+activateLazyVideo();
+
+// Function Count Up Stats On Appearing
+function countUpStats() {
+  const statsSection = document.querySelector('.stats');
+  const rect = statsSection.getBoundingClientRect();
+  const isVisible = window.innerHeight > rect.top && 0 < rect.bottom;
+  if (!isVisible) return;
+  const counts = statsSection.querySelectorAll('[data-count]');
+  counts.forEach((count) => {
+    const countUp = new CountUp(count, count.getAttribute('data-count'), {
+      duration: 5,
+      easing: 'linear',
+    });
+    countUp.start();
+  });
+  window.removeEventListener('scroll', countUpStats);
+}
+countUpStats();
+
+// Function Add Current Year To Footer
+function addYearToFooter() {
+  const footerYear = document.getElementById('year');
+  const date = new Date();
+  const year = date.getFullYear();
+  footerYear.textContent = year;
+}
+addYearToFooter();
+
+// Function Control The Scroll To Top Button Showing And Hiding
+function controlScrollToTopButton() {
+  if (window.scrollY >= 1000) {
+    scrollToTopButton.classList.add('show');
+  } else {
+    scrollToTopButton.classList.remove('show');
+  }
+}
+controlScrollToTopButton();
+
 // Events
-toggleMeue.addEventListener('click', () => {
-    linkList.classList.toggle('active');
+// Event Controlling Main Header
+window.addEventListener('scroll', controlMainHeader);
+
+// Event To Open And Close NavBar Menu on Clicking
+navBarTrigger.addEventListener('click', () => {
+  if (navBarTrigger.getAttribute('data-state') === 'closed') openNavBarMenu();
+  else closeNavBarMenu();
 });
 
-// Event Showing and Hidding The Scroll To Top Button
-window.onscroll = () => {
-    scrollToTopControl();
-};
+navBarLinks.forEach((link) =>
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    closeNavBarMenu();
+    const section = sections.find(
+      (section) => section.id === link.getAttribute('data-section')
+    );
+    section.scrollIntoView();
+  })
+);
+
+document.addEventListener('click', function (event) {
+  if (
+    navBarMenu.getAttribute('data-state') === 'open' &&
+    !navBar.contains(event.target)
+  ) {
+    closeNavBarMenu();
+  }
+});
+
+// Event Activating Lazy Video On Appearing
+window.addEventListener('scroll', activateLazyVideo);
+
+// Event Counting Up Stats On Appearing
+window.addEventListener('scroll', countUpStats);
+
+// Event Adding Active State To Nav Links On Section Appearing
+window.addEventListener('scroll', activateNavLinks);
 
 // Scrolling to Top On Clicking
-toTopButton.addEventListener('click', () => {
-    window.scrollTo({ top: 0 });
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0 });
 });
+
+// Event Showing and Hiding The Scroll To Top Button
+window.addEventListener('scroll', controlScrollToTopButton);
+
+// WOW
+const wow = new WOW({
+  animateClass: 'animated',
+  offset: 100,
+});
+wow.init();
+
+// Swiper Home
+const homeSwiper = new Swiper('.home', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.home .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.home .swiper-button-next',
+    prevEl: '.home .swiper-button-prev',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+// Swiper Testimonials
+const testimonialsSwiper = new Swiper('.testimonials-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.testimonials .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.testimonials .swiper-button-next',
+    prevEl: '.testimonials .swiper-button-prev',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+// Mixitup Portfolio
+const mixer = mixitup('.portfolio-items');
